a74f2a643c88ee5d853ba784e7aeec095ff294fc
AmodhShenoy <amodhshenoy@gmail.com>
2021-05-13 12:53:01 +0530

    Customer Index page done

--- organic_store/ADMIN PANEL/models/CustomerModels.php
+++ organic_store/ADMIN PANEL/models/CustomerModels.php
@@ -11,7 +11,15 @@ class CustomerModel
 
     public function getAllCustomers()
     {
-        $sql = 'SELECT * FROM customer;';
+        $sql = 'SELECT cx.cxid, cx.cxname, cx.cxemail, cx.cxphone,
+        IFNULL(o.cnt, 0) as ordercount, 
+        IFNULL(c.cnt,0) as cartcount, 
+        IFNULL(w.cnt, 0 ) as wishcount 
+        from customer cx 
+        left outer join ( select cxid, count(cxid) as cnt from wishlist group by cxid) w on w.cxid = cx.cxid 
+        left outer join ( select cxid, count(cxid) as cnt from cart group by cxid) c on c.cxid = cx.cxid 
+        left outer join (select cxid, count(cxid) as cnt from orders group by cxid) o on o.cxid = cx.cxid;';
+        // $sql = 'SELECT * FROM customer;';
         $prep = $this->db->prepare($sql);
         $prep->execute();
         $result = $prep->fetchAll();
