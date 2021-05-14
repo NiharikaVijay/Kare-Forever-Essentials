a065b414c4c09764ba229fc517e18dc2adc92bb8
AmodhShenoy <amodhshenoy@gmail.com>
2021-05-14 19:23:49 +0530

    Order Index page sorted

--- organic_store/ADMIN PANEL/models/OrderModels.php
+++ organic_store/ADMIN PANEL/models/OrderModels.php
@@ -11,7 +11,11 @@ class OrderModel
 
     public function getAllOrders()
     {
-        $sql = 'SELECT * FROM order;';
+        $sql = 'SELECT  o.ordid, o.timestamp,
+        c.cxname,
+        a.line1, a.line2, a.city, a.pincode, a.state,
+        o.amount, o.status
+        FROM orders o LEFT OUTER JOIN customer c ON c.cxid=o.cxid LEFT OUTER JOIN address a ON a.cxid=c.cxid;';
         $prep = $this->db->prepare($sql);
         $prep->execute();
         $result = $prep->fetchAll();
