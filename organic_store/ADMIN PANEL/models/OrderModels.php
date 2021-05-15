c57a87229196f5c609d6cf62eba73a6c14addac2
AmodhShenoy <amodhshenoy@gmail.com>
2021-05-15 21:57:46 +0530

    Minor cleaning

--- organic_store/ADMIN PANEL/models/OrderModels.php
+++ organic_store/ADMIN PANEL/models/OrderModels.php
@@ -21,4 +21,24 @@ class OrderModel
         $result = $prep->fetchAll();
         return $result;
     }
+    public function getOrderDetails(String $ordid)
+    {
+        $sql = 'SELECT  c.cxname, c.cxemail, c.cxphone,
+        o.timestamp, o.notes,
+        a.line1, a.line2, a.city, a.pincode, a.state,
+        o.amount, o.status
+        FROM orders o LEFT OUTER JOIN customer c ON c.cxid=o.cxid LEFT OUTER JOIN address a ON a.cxid=c.cxid WHERE o.ordid= :ordid;';
+        $prep = $this->db->prepare($sql);
+        $prep->execute(['ordid' => $ordid]);
+        $details = $prep->fetchAll();
+
+        $sql = 'SELECT  c.cxname, c.cxemail, c.cxphone,
+        o.timestamp, o.notes,
+        a.line1, a.line2, a.city, a.pincode, a.state,
+        o.amount, o.status
+        FROM orders o LEFT OUTER JOIN customer c ON c.cxid=o.cxid LEFT OUTER JOIN address a ON a.cxid=c.cxid WHERE o.ordid= :ordid;';
+        $prep = $this->db->prepare($sql);
+        $prep->execute(['ordid' => $ordid]);
+        $details = $prep->fetchAll();
+    }
 }
