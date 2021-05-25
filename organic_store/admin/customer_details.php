2d7066fa8d6e767a5f0c900e25ca97fe50645fdc
AmodhShenoy <amodhshenoy@gmail.com>
2021-05-25 19:55:52 +0530

    Login and logout added

--- organic_store/admin/customer_details.php
+++ organic_store/admin/customer_details.php
@@ -5,6 +5,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: login.php');
+	exit;
+}
+
 require __DIR__ . '/vendor/autoload.php';
 include 'models/CustomerModels.php';
 
