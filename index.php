66ca94234c6d5413e2b15dcdf02cff0a3f839aa8
AmodhShenoy <amodhshenoy@gmail.com>
2021-06-11 12:32:01 +0530

    Home page added

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+session_start();
+
+if (!isset($_SESSION['cxid'])) {
+    include __DIR__ . '/models/user/HelperModels.php';
+
+    $helperModel = new HelperModel($db);
+    $_SESSION['cxloggedin'] = false;
+    $_SESSION['cxid'] = $helperModel->generateRandomString(5);
+}
+
+# TODO To be removed during deployment
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/vendor/autoload.php';
+#Include whatever model you want here
+include __DIR__ . '/models/user/CustomerModels.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+#Setting up Twig
+$loader = new FilesystemLoader(__DIR__ . '/templates/user');
+$twig = new Environment($loader);
+
+#Setting up PDO
+$dbname = 'kare2';
+$host = 'localhost';
+$port = '3306';
+$charset = 'utf8';
+$username = 'root';
+$password = '';
+$db = new PDO('mysql:dbname=' . $dbname . ';host=' . $host . ';port=' . $port . ';charset=' . $charset, $username, $password);
+$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+
+#Main code, changes for every view
+$customerModel = new CustomerModel($db);
+
+// $products = $productModel->getProductsByCategory('cat4');
+
+// echo $twig->render('menu.twig', [
+//     'account' => $_SESSION,
+//     'title' => 'Special Kare',
+//     'banner' => '/media/categories/specialkare.jpg',
+//     'products' => $products['products'],
+//     'concerns' => $products['concerns']
+// ]);
+
+echo $twig->render('home.twig');
