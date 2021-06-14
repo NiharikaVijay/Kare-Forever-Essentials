6a546124153b5944b5d2a6bca282c13b98e5270d
AmodhShenoy <amodhshenoy@gmail.com>
2021-06-14 11:53:15 +0530

    Dummy dashboard added to admin

--- admin/login.php
+++ admin/login.php
@@ -5,7 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require dirname(dirname(__FILE__)). '/vendor/autoload.php';
+session_start();
+require dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+include dirname(dirname(__FILE__)) . '/models/admin/AdminModels.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
@@ -25,7 +27,30 @@ $db = new PDO('mysql:dbname=' . $dbname . ';host=' . $host . ';port=' . $port . 
 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
-echo $twig->render('login.twig');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    echo $twig->render('login.twig');
+} else {
+    $adminModel = new AdminModel($db);
 
-// echo password_hash("password", PASSWORD_DEFAULT);
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
 
+    $admin = $adminModel->verify($email, $pwd);
+
+    // print_r($_POST);
+    if ($admin['verified']) {
+        session_regenerate_id();
+        $_SESSION['loggedin'] = TRUE;
+        $_SESSION['admid'] = $admin['details'][0][0];
+        $_SESSION['name'] = $admin['details'][0][1];
+        $_SESSION['email'] = $admin['details'][0][2];
+        $_SESSION['phone'] = $admin['details'][0][3];
+        $_SESSION['avatar'] = $admin['details'][0][5];
+
+        header("Location: /admin");
+        die();
+    }
+
+    header("Location: /admin");
+    die();
+}
